ca1b670175d06c14a42dd807beb57701eaa570b2
Gergo <zoldy775@gmail.com>
2022-01-02 03:09:36 +0100

    search extended with genre

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,11 +2,22 @@ import {FC, useEffect, useState } from "react";
 
 const Home:FC = () => {
   const [searchedMovie, setSearchedMovie] = useState<string>("");
+  const [genres, setGenres] = useState<[]>();
   const [searchResults, setSearchResults] = useState<[]>();
   const apiKey = "1c5abaaeaa13c66b570ad3042a0d51f4";
 
   useEffect(() => {
-    
+    async function genreFetch() {
+      const url = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + apiKey;
+
+      const response = await fetch(url);
+      const data = response.json();
+      const results = data.then(response => {
+        setGenres(response.genres);
+      }).catch(error => console.log(error));
+    }
+
+    genreFetch();
   },[]);
 
   const handleSearch = (value: string) => {
@@ -24,7 +35,6 @@ const Home:FC = () => {
       const response = await fetch(query);
       const data = response.json();
       const results = data.then(response => {
-        console.log(response.results);
         setSearchResults(response.results);
       }).catch(error => console.log(error));
     }
@@ -32,13 +42,32 @@ const Home:FC = () => {
     movieFetch();
   }
 
+  const renderGenre = (result: any) => {
+    const filteredGenres:any = [];
+    genres?.filter((genre: any) => {
+      result.genre_ids.map((id: number) => {
+        if (genre.id === id) {
+          filteredGenres.push(genre.name);
+        }
+      })
+    });
+    
+    return filteredGenres.map((genre: any, index: number) => {
+      return <p key={index}>{genre}</p>;
+    });
+  }
+
   const handleSearchResults = () => {
     console.log(searchResults);
-    return searchResults && searchResults.map((result: any, index) => (
-      <div key={index}>
+    return searchResults && searchResults.map((result: any, index) => {
+      
+      return <div key={index}>
+        <img src={result.poster_path} alt="" />
         <div>{result.title}</div>
+        <div>{result.release_date}</div>
+        <div style={{ display: "flex", justifyContent: "center" }}>{renderGenre(result)}</div>
       </div>
-    ))
+    })
   }
 
   return <div>
